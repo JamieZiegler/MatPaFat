0c80bb6d84171abd33f5ffa83fdfdbca3bd236a8
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2026-01-11 17:54:52 +0100

    fixed nav-link routing issue

--- Mat-Pa-Fat/src/components/Header.jsx
+++ Mat-Pa-Fat/src/components/Header.jsx
@@ -2,15 +2,22 @@ import { Link, useLocation } from 'react-router-dom'
 
 export default function Header() {
     const location = useLocation()
+    const isHome = location.pathname === '/'
 
     return (
         <>
         <header>
             <nav aria-label="Huvudnavigation">
                 <ul>
-                    <li className="nav-link"><a href="#introduction">Introduktion</a></li>
-                    <li className="nav-link"><a href="#services">Tjänster</a></li>
-                    <li className="nav-link"><a href="#contact">Kontakt</a></li>
+                    <li className="nav-link">
+                        {isHome ? <a href="#introduction">Introduktion</a> : <Link to="/#introduction">Introduktion</Link>}
+                    </li>
+                    <li className="nav-link">
+                        {isHome ? <a href="#services">Tjänster</a> : <Link to="/#services">Tjänster</Link>}
+                    </li>
+                    <li className="nav-link">
+                        {isHome ? <a href="#contact">Kontakt</a> : <Link to="/#contact">Kontakt</Link>}
+                    </li>
                     <li className="nav-link"><Link to="/villkor" state={{ from: location.hash }}>Betalning & villkor</Link></li>
                 </ul>
             </nav>
