fa8304988b17c70c25d65c2391ad5c5ba0d8ca47
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2026-01-31 16:08:28 +0100

    Update icons and hero for launch

--- Mat-Pa-Fat/src/components/Hero.jsx
+++ Mat-Pa-Fat/src/components/Hero.jsx
@@ -9,35 +9,12 @@ export default function Hero() {
 
     return (
         <>
-            {/* Innan verksamheten öppnar: */}
             <div className="hero-wrapper">
-
-                {/* <section className="pre-opening-hero" aria-labelledby="hero-tagline">
-                    <div className="hero-content">
-                        <img className="hero-logo-small" src={logo} alt="Mat På Fat Bergslagen logotyp" width="280" height="200" fetchPriority="high" />
-                        <h1 className="sr-only">Catering i Lindesberg</h1>
-                        <div className="hero-info">
-                            <p>
-                                Jag förbereder just nu för att öppna mina dörrar och börja servera dig läckra, näringsrika matlådor och skräddarsydd catering.
-                            </p>
-                            <p>
-                                Kontakta mig redan nu för prisförslag och information!
-                            </p>
-                            <div className="cta-contact">
-                                <button className="hero-contact-btn btn" onClick={scrollToContact} aria-label="Gå till kontaktformuläret">Kontakt</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="hero-countdown-wrapper">
-                        <Countdown />
-                    </div>
-                </section> */}
-
-                {/* Avskommentera när verksamheten öppnat */}
                 <section className="open-hero" aria-labelledby="hero-tagline">
                     <div className="hero-content">
-                        <img className="hero-logo-small" src={logo} alt="Mat På Fat Bergslagen logotyp" width="280" height="200" fetchPriority="high" />
+                        <img className="hero-logo-small" src={logo} alt="Mat På Fat Bergslagen logotyp" fetchPriority="high" />
                         <h1 className="sr-only">Catering i Lindesberg</h1>
+                        <h1 id="hero-tagline" className="hero-tagline">Hemlagad mat med kärlek</h1>
                         <div className="hero-info">
                             <p>
                                 Kontakta mig för prisförslag och information!
