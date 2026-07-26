886060641f044f209746443f24ae87e77b2b6e8a
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2026-07-26 20:09:18 +0200

    Added summer hero

--- Mat-Pa-Fat/src/components/Hero.jsx
+++ Mat-Pa-Fat/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import logo from '../assets/images/logo/mpf-genomskinlig-for-webb.svg'
-import closedImg from '../assets/images/temp_hero_logo_shape.svg';
+import closedImg from '../assets/images/temp_hero_summer2.png';
 // import closedImg from '../assets/images/temp_hero.jpg';
 // import Countdown from './CountDown'
 
@@ -30,11 +30,11 @@ export default function Hero() {
                     <div className={`hero-slide ${!showLogo ? 'active' : 'inactive'}`}>
                         <div className="hero-content">
                             <h1 id="hero-tagline" className="hero-tagline">Välkommen till Mat på Fat!</h1>
-                            <img className="hero-logo-small" src={closedImg} alt="Stängt vecka 11" fetchPriority="high" />
+                            <img className="hero-logo-small" src={closedImg} alt="Stängt för sommarsemester vecka 31 - 33. Tack för förståeslen!" fetchPriority="high" />
                             <div className="hero-info">
-                                <h2>Tack för en fantastisk start!</h2>
+                                <h2>Tack för en fantastisk vår och sommar!</h2>
                                 <div className="hero-info">
-                                    <p>Nu tar jag en kort paus för att ladda batterierna i skidbacken. Åter öppet måndag v.12!</p>
+                                    <p>Nu tar jag några veckors sommarsemester. Åter öppet måndag v.34!</p>
                                     <div className="cta-contact">
                                         <button className="hero-contact-btn btn" onClick={scrollToContact} aria-label="Gå till kontaktformuläret">Kontakt</button>
                                     </div>
